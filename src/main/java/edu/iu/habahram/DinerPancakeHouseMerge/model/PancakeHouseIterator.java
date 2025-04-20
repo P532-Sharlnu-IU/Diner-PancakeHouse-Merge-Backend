06e043d94ac2c9c11e6ad4317b2e157327c2d353
LNU, Sharath <sharlnu@iu.edu>
2025-04-20 12:28:16 -0400

    Removed Diner and Panc Iterator

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/model/PancakeHouseIterator.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/model/PancakeHouseIterator.java
@@ -1,31 +1,31 @@
-package edu.iu.habahram.DinerPancakeHouseMerge.model;
-
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
-
-
-//-->NOT IN USE
-
-public class PancakeHouseIterator implements Iterator<MenuItem> {
-    List<MenuItem> items;
-    int position = 0;
-
-    public PancakeHouseIterator(List<MenuItem> items) {
-        this.items = items;
-    }
-
-    public MenuItem next() {
-        MenuItem menuItem = items.get(position);
-        position = position + 1;
-        return menuItem;
-    }
-
-    public boolean hasNext() {
-        if (position >= items.size() || items.get(position) == null) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-}
+//package edu.iu.habahram.DinerPancakeHouseMerge.model;
+//
+//import java.util.Iterator;
+//import java.util.List;
+//import java.util.NoSuchElementException;
+//
+//
+////-->NOT IN USE
+//
+//public class PancakeHouseIterator implements Iterator<MenuItem> {
+//    List<MenuItem> items;
+//    int position = 0;
+//
+//    public PancakeHouseIterator(List<MenuItem> items) {
+//        this.items = items;
+//    }
+//
+//    public MenuItem next() {
+//        MenuItem menuItem = items.get(position);
+//        position = position + 1;
+//        return menuItem;
+//    }
+//
+//    public boolean hasNext() {
+//        if (position >= items.size() || items.get(position) == null) {
+//            return false;
+//        } else {
+//            return true;
+//        }
+//    }
+//}
